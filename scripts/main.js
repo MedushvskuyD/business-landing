91dbc5bb4e7058dd1d7deedb8abc5cc22559b1a5
Deman250 <deman.250052@gmail.com>
2019-11-18 03:34:37 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function() {
-  $('.slider__inner').slick({
+  $('.slider__inner, .news__slider-inner').slick({
     nextArrow: '<button type="button" class="slick slick-next"></button>',
     prevArrow: '<button type="button" class="slick slick-prev"></button>',
     infinite: false,
